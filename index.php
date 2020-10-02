bdd216b8028bc03a493fc6554790039c7fd02a0c
Devender00999 <Devender00999@gmail.com>
2020-10-02 15:04:17 +0530

    Adding New Files to Responsive NavBar

---
+++ index.php
@@ -0,0 +1,29 @@
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NavBar</title>
+</head>
+<style>
+    nav{
+        display:flex;
+        justify-content:space-between;
+    }
+    nav ul li{
+        float:left;
+    }
+</style>
+<body>
+    <nav>
+        <label for="logo"><h4>Graphics4U</h4></label>
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About Us</li>
+            <li>Our Projects</li>
+            <li>Our Team</li>
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NavBar</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About Us</li>
+            <li>Our Projects</li>
+            <li>Our Team</li>
+        </ul>
+    </nav>
+</body>
+</html>
